f673459c1b6efec4539c9d5fda03e4a1dd1936eb
Tolani99 <tolaniadisa999@gmail.com>
2023-10-06 17:04:44 +0100

    finding all the authors that viewed at least one of their own articles

--- sql/cracking_sql_interview_in_50_questions/article_views_1.sql
+++ sql/cracking_sql_interview_in_50_questions/article_views_1.sql
@@ -9,6 +9,7 @@ Table: Views
 | viewer_id     | int     |
 | view_date     | date    |
 +---------------+---------+
+
 There is no primary key (column with unique values) for this table, the table may have duplicate rows.
 Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
 Note that equal author_id and viewer_id indicate the same person.
@@ -45,4 +46,35 @@ Output:
 | 7    |
 +------+
 */
+# Write your MySQL query statement below
 
+SELECT DISTINCT author_id id FROM Views 
+WHERE author_id = viewer_id 
+ORDER BY id ASC
+
+/*
+Accepted
+Runtime: 120 ms
+Case 1
+Input
+Views =
+| article_id | author_id | viewer_id | view_date  |
+| ---------- | --------- | --------- | ---------- |
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |...
+View all
+Output
+| id |
+| -- |
+| 4  |
+| 7  |
+Expected
+| id |
+| -- |
+| 4  |
+| 7  |
+*/
